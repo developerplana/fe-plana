94631024acfeeb204e2984c09f6b1e910ed1426e
Fauzan <fauzanhabib20@gmail.com>
2025-04-08 21:26:56 +0700

    update slickJS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Social from '../components/Social';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +29,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Navbar />
+        <Social/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
+      <Footer/>
     </html>
   );
 }
