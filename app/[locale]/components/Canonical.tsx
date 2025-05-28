504a1b55d6c7570e8fa84c83cf3586acfafcad16
Fauzan <fauzanhabib20@gmail.com>
2025-05-28 14:00:19 +0700

    update canonical

--- app/[locale]/components/Canonical.tsx
+++ app/[locale]/components/Canonical.tsx
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 
 export default function Canonical() {
   const pathname = usePathname();
-  const canonicalUrl = `${process.env.NEXT_PUBLIC_LARAVEL_BASE_URL}${pathname}`;
+  const canonicalUrl = `${process.env.BASE_URL}${pathname}`;
 
   return (
       <link rel="canonical" href={canonicalUrl} />
