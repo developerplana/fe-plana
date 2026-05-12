6bcd1201fc6c10ce2e6b887b446f6428fc47c4fc
Fauzan Habib <fauzan@kct.co.id>
2026-05-12 08:02:16 +0700

    update canonical

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,20 @@
-
+import type { Metadata } from "next";
 import HomePage from "./homepage/page";
 
-export default function Home() {
-    return (
-      <>
-        <HomePage />
-      </>
-    );
-  }
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://plana.vision';
   
+  return {
+    alternates: {
+      canonical: baseUrl,
+    },
+  };
+}
+
+export default function Home() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
